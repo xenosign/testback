4917ff6559e1947f0dedb54951dc567286d3aff4
kdtTetz <xenosign@naver.com>
2025-02-19 02:13:19 +0900

    Initial commit

---
+++ src/main/java/com/tetz/testback/TestbackApplication.java
@@ -0,0 +1,13 @@
+package com.tetz.testback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tetz/testback/TestbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetz.testback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
